602c514f7e702651912d664d069f452f54daa6f6
= <anton.shheglov.1@gmail.com>
2019-11-14 20:32:36 +0300

    add: module.cs with static method Reader

---
+++ NET.W.2019.Shchahlou.1/MultiFilesAssembly/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiFilesAssembly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Module.Reader());
+        }
+    }
+}
