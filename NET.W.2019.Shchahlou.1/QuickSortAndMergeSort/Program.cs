ff8a7f662cae5ee1a48cef7ab42cd5648667123e
= <anton.shheglov.1@gmail.com>
2019-11-14 22:05:17 +0300

    add: checks resualt of sorting(show it)

--- NET.W.2019.Shchahlou.1/QuickSortAndMergeSort/Program.cs
+++ NET.W.2019.Shchahlou.1/QuickSortAndMergeSort/Program.cs
@@ -8,10 +8,13 @@ namespace QuickSortAndMergeSort
         {
             int[] arr = { 4, 7, 3, 5, 8 };
             QuickSort.Sort(arr, 0, arr.Length - 1);
+            int[] newArr = { 4, 7, 3, 5, 8 };
+            MergeSort.Sort(newArr);
             Console.WriteLine();
             for (int i = 0; i < arr.Length; i++)
             {
-               Console.Write("{0} ", arr[i]);
+                Console.WriteLine("{0}-{1} quick", arr[i], i);
+                Console.WriteLine("{0}-{1} merge", newArr[i], i);
             }
             Console.ReadLine();
 
