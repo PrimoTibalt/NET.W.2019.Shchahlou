3acb90e692666678afc39a03edc331483b28ba42
= <anton.shheglov.1@gmail.com>
2019-11-16 17:00:06 +0300

    add: function InsertNumbers

---
+++ NET.W.2019.Shchahlou.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+
+namespace NET.W._2019.Shchahlou._2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static long? InsertNumbers(int numberSource, int numberIn, int i, int j)
+        {
+            BitArray bitSource = new BitArray(new int[] { numberSource });
+            BitArray bitIn = new BitArray(new int[] { numberIn });
+            if (i > j)
+                return null;
+            
+            for (int num = i; num < j ; num++)
+            {
+                bitSource[num] = bitIn[num - i];
+            }
+            int[] zeroIsResult = new int[1];
+            bitSource.CopyTo(zeroIsResult, 0);
+            return zeroIsResult[0];
+        }
+    }
+}
