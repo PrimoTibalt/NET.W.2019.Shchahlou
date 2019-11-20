939bc22e7f25d1b31959858d8402075727e65c3d
= <anton.shheglov.1@gmail.com>
2019-11-20 21:41:38 +0300

    add: comments for methods and BinaryEuclidGCD(I also wrote in last commit to tests, that I implemented this algorithm, but I didnt commit this file)

--- NET.W.2019.Shchahlou.4/GCD.cs
+++ NET.W.2019.Shchahlou.4/GCD.cs
@@ -4,14 +4,25 @@ using System.Text;
 
 namespace NET.W._2019.Shchahlou._4
 {
-    static class GCD
+    public static class GCD
     {
+        /// <summary>
+        /// Use Euclid algorithms to find GCD of any count of input numbers.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns>GCD</returns>
         public static int EuclidGCD(params int[] numbers)
         {
             for (int num = 1; num < numbers.Length; num++)
-            {
+            { 
                 int a = numbers[num];
-                int b = numbers[num - 1];
+                int b = numbers[num-1];
+                /* GCD(0,v) == v; GCD(u,0) == u, GCD(0,0) == 0 */
+                if (a == 0) return a;
+                if (b == 0) return b;
+
+                if (a < 0 || b < 0)
+                    throw new ArgumentException("Null or negative number in input array!");
                 while (a != b)
                 {
                     if (a > b)
@@ -20,6 +31,62 @@ namespace NET.W._2019.Shchahlou._4
                         b -= a;
                 }
                 numbers[num] = a;// a = b
+            }
+            return numbers[numbers.Length - 1];
+        }
+        /// <summary>
+        /// Use binary Euclid algorithms to find GCD of any count of input numbers.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns>GCD</returns>
+        public static int BinaryEuclidGCD(params int[] numbers)
+        {
+            for (int num = 1; num < numbers.Length; num++)
+            {
+                int a = numbers[num];
+                int b = numbers[num - 1];
+                int shift = 0;
+
+                /* GCD(0,v) == v; GCD(u,0) == u, GCD(0,0) == 0 */
+                if (a == 0) return a;
+                if (b == 0) return b;
+
+                /* Let shift = lg K, where K is the greatest power of 2
+                    dividing both u and v. */
+                while (((a | b) & 1) == 0)
+                {
+                    shift++;
+                    a >>= 1;
+                    b >>= 1;
+                }
+
+                while ((a & 1) == 0)
+                    a >>= 1;
+
+                /* From here on, u is always odd. */
+                do
+                {
+                    /* remove all factors of 2 in v -- they are not common */
+                    /*   note: v is not zero, so while will terminate */
+                    while ((b & 1) == 0)
+                        b >>= 1;
+
+                    /* Now u and v are both odd. Swap if necessary so u <= v,
+                        then set v = v - u (which is even). For bignums, the
+                         swapping is just pointer movement, and the subtraction
+                          can be done in-place. */
+                    if (a > b)
+                    {
+                        int t = b;
+                        b = a;
+                        a = t; // Swap u and v.
+                    }
+
+                    b -= a; // Here v >= u.
+                } while (b != 0);
+
+                /* restore common factors of 2 */
+                numbers[num] = a << shift;
             }
             return numbers[numbers.Length - 1];
         }
