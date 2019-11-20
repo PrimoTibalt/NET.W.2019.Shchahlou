9d53175c8069a19c6be454ae785ff0943266280f
= <anton.shheglov.1@gmail.com>
2019-11-20 20:18:02 +0300

    add: static method GCD with method EuclidGCD(calculate gcd of any count of numbers)

---
+++ NET.W.2019.Shchahlou.4/GCD.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.W._2019.Shchahlou._4
+{
+    static class GCD
+    {
+        public static int EuclidGCD(params int[] numbers)
+        {
+            for (int num = 1; num < numbers.Length; num++)
+            {
+                int a = numbers[num];
+                int b = numbers[num - 1];
+                while (a != b)
+                {
+                    if (a > b)
+                        a -= b;
+                    else
+                        b -= a;
+                }
+                numbers[num] = a;// a = b
+            }
+            return numbers[numbers.Length - 1];
+        }
+    }
+}
---
+++ NET.W.2019.Shchahlou.4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NET.W._2019.Shchahlou._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
