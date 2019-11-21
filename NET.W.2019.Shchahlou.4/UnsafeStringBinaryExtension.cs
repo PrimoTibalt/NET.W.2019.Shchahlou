5a812b734130765f920cba70fd0fefd513c6b7f0
= <anton.shheglov.1@gmail.com>
2019-11-21 18:19:54 +0300

    feat: functionality is the same, rewrite UnsafeBinaryString, now it makes string by custom logic, not by Convertors strucktures

--- NET.W.2019.Shchahlou.4/UnsafeStringBinaryExtension.cs
+++ NET.W.2019.Shchahlou.4/UnsafeStringBinaryExtension.cs
@@ -6,9 +6,22 @@ namespace NET.W._2019.Shchahlou._4
 {
     public static class DoubleExtension
     {
+        /// <summary>
+        /// Return binary version of the input double.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>string of 63 bits</returns>
         unsafe public static string UnsafeBinaryString(this double number)
         {
-            string result = Convert.ToString(BitConverter.DoubleToInt64Bits(number), 2);
+            long temp;
+            string result = string.Empty;
+            long* lptr = (long*)&number;
+            temp = *lptr;
+
+            for(int i = 63; i >= 0; i--)
+            {
+                result += (((temp >> i) & 1) == 0) ? "0" : "1";
+            }
             return result;
         }
     }
