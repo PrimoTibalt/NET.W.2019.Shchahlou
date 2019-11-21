a6cafa764133d75c7b77fd43fbc380a638217584
= <anton.shheglov.1@gmail.com>
2019-11-21 10:33:02 +0300

    add: extension method for double UnsafeBinaryString(need to rewrite without convertors) and Tests for him

---
+++ NET.W.2019.Shchahlou.4/UnsafeStringBinaryExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.W._2019.Shchahlou._4
+{
+    public static class DoubleExtension
+    {
+        unsafe public static string UnsafeBinaryString(this double number)
+        {
+            string result = Convert.ToString(BitConverter.DoubleToInt64Bits(number), 2);
+            return result;
+        }
+    }
+}
--- Tests/TestDay4.cs
+++ Tests/TestDay4.cs
@@ -48,5 +48,48 @@ namespace Tests
             Assert.AreEqual(3, v3);
             Assert.AreEqual(1, v1);
         }
+
+        [TestMethod]
+        public void TestUnsafeBinaryString()
+        {
+            //arrange
+            double value0 = -255.255;
+            double value1 = 255.255;
+            double value2 = 4294967295.0;
+            double value3 = double.MinValue;
+            double value4 = double.MaxValue;
+            double value5 = double.Epsilon;
+            double value6 = double.NaN;
+            double value7 = double.NegativeInfinity;
+            double value8 = double.PositiveInfinity;
+            double value9 = -0.0;
+            double value10 = 0.0;
+
+            //act
+            string result0 = value0.UnsafeBinaryString();
+            string result1 = value1.UnsafeBinaryString();
+            string result2 = value2.UnsafeBinaryString();
+            string result3 = value3.UnsafeBinaryString();
+            string result4 = value4.UnsafeBinaryString();
+            string result5 = value5.UnsafeBinaryString();
+            string result6 = value6.UnsafeBinaryString();
+            string result7 = value7.UnsafeBinaryString();
+            string result8 = value8.UnsafeBinaryString();
+            string result9 = value9.UnsafeBinaryString();
+            string result10 = value10.UnsafeBinaryString();
+
+            //assert
+            Assert.AreEqual("1100000001101111111010000010100011110101110000101000111101011100", result0);
+            Assert.AreEqual("0100000001101111111010000010100011110101110000101000111101011100", result1);
+            Assert.AreEqual("0100000111101111111111111111111111111111111000000000000000000000", result2);
+            Assert.AreEqual("1111111111101111111111111111111111111111111111111111111111111111", result3);
+            Assert.AreEqual("0111111111101111111111111111111111111111111111111111111111111111", result4);
+            Assert.AreEqual("0000000000000000000000000000000000000000000000000000000000000001", result5);
+            Assert.AreEqual("1111111111111000000000000000000000000000000000000000000000000000", result6);
+            Assert.AreEqual("1111111111110000000000000000000000000000000000000000000000000000", result7);
+            Assert.AreEqual("0111111111110000000000000000000000000000000000000000000000000000", result8);
+            Assert.AreEqual("1000000000000000000000000000000000000000000000000000000000000000", result9);
+            Assert.AreEqual("0000000000000000000000000000000000000000000000000000000000000000", result10);
+        }
     }
 }
