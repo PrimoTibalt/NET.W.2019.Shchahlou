18a95a1a72ab905ac4845fe221ee0cff708c1c72
= <anton.shheglov.1@gmail.com>
2019-11-26 07:52:33 +0300

    add: Polynom.cs file, skeleton of Polynom class, embody ToString method

---
+++ NET.W.2019.Shchahlou.6/Polynomial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.W._2019.Shchahlou._6
+{
+    public sealed class Polynomial
+    {
+        private double[] polynom;
+        
+        public Polynomial(params double[] coef)
+        {
+            polynom = coef;
+        }
+        public override bool Equals(object pol)
+        {
+            return base.Equals(pol);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder strPolynom = new StringBuilder();
+            for(int power = 0; power < polynom.Length; power++)
+            {
+                strPolynom.Append($"{polynom[power]}x^{power}");
+                if (power != polynom.Length - 1)
+                    strPolynom.Append("+");
+                else
+                    strPolynom.Append("=0");
+            }
+            return strPolynom.ToString();
+        }
+    }
+}
