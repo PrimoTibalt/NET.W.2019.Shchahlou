f58faf01853a9164166a4984d57369ea225dd836
= <anton.shheglov.1@gmail.com>
2019-11-26 14:14:06 +0300

    add: BubbleSort file and skeleton of BubbleSort struct.

---
+++ NET.W.2019.Shchahlou.6/BubbleSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.W._2019.Shchahlou._6
+{
+    /// <summary>
+    /// Structure with methods to sort jugged array by some feature.
+    /// </summary>
+    public struct BubbleSort
+    {
+        public int[][] AscendingSumOfLines(params int[][] M)
+        {
+            return new int[M.Length][];
+        }
+
+        public int[][] DecreasingSumOfLines(params int[][] M)
+        {
+            return new int[M.Length][];
+        }
+
+        public int[][] AscendingMaxElementValue(params int[][] M)
+        {
+            return new int[M.Length][];
+        }
+
+        public int[][] DecreasingMaxElementValue(params int[][] M)
+        {
+            return new int[M.Length][];
+        }
+
+
+        public int[][] AscendingMinElementValue(params int[][] M)
+        {
+            return new int[M.Length][];
+        }
+
+        public int[][] DecreasingMinElementValue(params int[][] M)
+        {
+            return new int[M.Length][];
+        }
+    }
+}
