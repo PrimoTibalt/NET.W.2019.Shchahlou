f2e4d7ccebe4dad61111610cc5f80e0ebafc9c4b
= <anton.shheglov.1@gmail.com>
2019-11-26 18:47:14 +0300

    add: struct BubbleSort, array.Sum in BSHelper.

---
+++ NET.W.2019.Shchahlou.6/BSHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.W._2019.Shchahlou._6
+{
+    public static class BSHelper
+    {
+        public static int Sum(this int[] arr)
+        {
+            int sum = 0;
+            foreach(int a in arr)
+            {
+                sum += a;
+            }
+            return sum;
+        }
+    }
+}
--- NET.W.2019.Shchahlou.6/BubbleSort.cs
+++ NET.W.2019.Shchahlou.6/BubbleSort.cs
@@ -9,35 +9,105 @@ namespace NET.W._2019.Shchahlou._6
     /// </summary>
     public struct BubbleSort
     {
+        delegate bool DoBubble(int[] n1, int[] n2);
+        private static void Bubble(ref int[] obj1, ref int[] obj2)
+        {
+            int[] temp = obj1;
+            obj1 = obj2;
+            obj2 = temp;
+        }
+
+        private static void Sort(DoBubble inCase, ref int[][] M)
+        {
+            int Len = M.Length;
+            for (int i = 0; i < Len; i++)
+                for (int j = 1; j < Len; j++)
+                    if (inCase.Invoke(M[j], M[j-1]))
+                        Bubble(ref M[j], ref M[j - 1]);
+        }
         public int[][] AscendingSumOfLines(params int[][] M)
         {
-            return new int[M.Length][];
+            bool BiggestSum(int[] arr1, int[] arr2)
+            {
+                return arr1.Sum() > arr2.Sum();
+            }
+            Sort(BiggestSum, ref M);
+            return M;
         }
 
         public int[][] DecreasingSumOfLines(params int[][] M)
         {
-            return new int[M.Length][];
+            bool SmallestSum(int[] arr1, int[] arr2)
+            {
+                return arr1.Sum() < arr2.Sum();
+            }
+            Sort(SmallestSum, ref M);
+            return M;
         }
 
         public int[][] AscendingMaxElementValue(params int[][] M)
         {
-            return new int[M.Length][];
+            bool BiggestElementIncrease(int[] arr1, int[] arr2)
+            {
+                int biggest1 = Int32.MinValue;
+                int biggest2 = Int32.MinValue;
+                foreach(int val1 in arr1)
+                    biggest1 = (val1 > biggest1) ? val1 : biggest1;
+                foreach (int val2 in arr2)
+                    biggest2 = (val2 > biggest2) ? val2 : biggest2;
+                return biggest1 > biggest2;
+            }
+            Sort(BiggestElementIncrease, ref M);
+            return M;
         }
 
         public int[][] DecreasingMaxElementValue(params int[][] M)
         {
-            return new int[M.Length][];
+            bool BiggestElementDecrease(int[] arr1, int[] arr2)
+            {
+                int biggest1 = Int32.MinValue;
+                int biggest2 = Int32.MinValue;
+                foreach (int val1 in arr1)
+                    biggest1 = (val1 > biggest1) ? val1 : biggest1;
+                foreach (int val2 in arr2)
+                    biggest2 = (val2 > biggest2) ? val2 : biggest2;
+                return biggest1 < biggest2;
+            }
+            Sort(BiggestElementDecrease, ref M);
+            return M;
         }
 
 
         public int[][] AscendingMinElementValue(params int[][] M)
         {
-            return new int[M.Length][];
+            bool MinElementIncrease(int[] arr1, int[] arr2)
+            {
+                int smallest1 = Int32.MaxValue;
+                int smallest2 = Int32.MaxValue;
+                foreach (int val1 in arr1)
+                    smallest1 = (val1 > smallest1) ? smallest1 : val1;
+                foreach (int val2 in arr2)
+                    smallest2 = (val2 > smallest2) ? smallest2 : val2;
+                return smallest1 > smallest2;
+            }
+            Sort(MinElementIncrease, ref M);
+            return M;
         }
 
         public int[][] DecreasingMinElementValue(params int[][] M)
         {
-            return new int[M.Length][];
+            bool MinElementDecrease(int[] arr1, int[] arr2)
+            {
+                int smallest1 = Int32.MaxValue;
+                int smallest2 = Int32.MaxValue;
+                foreach (int val1 in arr1)
+                    smallest1 = (val1 > smallest1) ? smallest1 : val1;
+                foreach (int val2 in arr2)
+                    smallest2 = (val2 > smallest2) ? smallest2 : val2;
+                return smallest1 > smallest2;
+            }
+            Sort(MinElementDecrease, ref M);
+            return M;
         }
     }
 }
