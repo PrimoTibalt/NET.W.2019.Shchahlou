0c2ec8928ede137ec95078f1f1adb26b217e7722
= <anton.shheglov.1@gmail.com>
2019-12-01 14:38:10 +0300

    add: project of 8's day homework.

---
+++ NET.W.2019.Shchahlou.8/Book.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.W._2019.Shchahlou._8
+{
+    class Book
+    {
+    }
+}
---
+++ NET.W.2019.Shchahlou.8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NET.W._2019.Shchahlou._8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
