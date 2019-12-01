7e2ff00cf08d7489d9c2a83bcc91ca5351ec018b
= <anton.shheglov.1@gmail.com>
2019-12-01 14:41:52 +0300

    add: BookCoparer's classes for cost and year.

---
+++ NET.W.2019.Shchahlou.8/BookComparer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.W._2019.Shchahlou._8
+{
+    class BookYearComparer<T> : IComparer<T>
+        where T : Book
+    {
+        public int Compare(T x, T y)
+        {
+            return x.Year.CompareTo(y.Year);
+        }
+    }
+
+    class BookCostComparer<T> : IComparer<T>
+        where T : Book
+    {
+        public int Compare(T x, T y)
+        {
+            return x.Cost.CompareTo(y.Cost);
+        }
+    }
+
+    //Could be more classes but what reason?
+}
