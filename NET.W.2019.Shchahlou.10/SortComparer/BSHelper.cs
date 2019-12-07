417f3614ea092501c0f1c10918dbd988feb0ea2f
= <anton.shheglov.1@gmail.com>
2019-12-07 10:30:00 +0300

    add: BubbleSort from the 6th task.

---
+++ NET.W.2019.Shchahlou.10/SortComparer/BSHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.W._2019.Shchahlou._10
+{
+    public static class BSHelper
+    {
+        public static int Sum(this int[] arr)
+        {
+            int sum = 0;
+            foreach (int a in arr)
+            {
+                sum += a;
+            }
+
+            return sum;
+        }
+    }
+}
