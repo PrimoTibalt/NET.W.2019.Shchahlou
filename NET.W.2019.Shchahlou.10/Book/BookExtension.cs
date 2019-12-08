63a295bdaec6c29b7fe4beb74d34a00860cc5f03
= <anton.shheglov.1@gmail.com>
2019-12-08 21:07:43 +0300

    add: extension for Book it changes format of result of ToString.

---
+++ NET.W.2019.Shchahlou.10/Book/BookExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NET.W._2019.Shchahlou._10
+{
+    public static class BookExtension
+    {
+        public static string ToString(this Book bk, string format, IFormatProvider provider, bool hollyDay)
+        {
+            if (hollyDay)
+            {
+                return $"****{ bk.ToString(format) }*****";
+            }
+            else
+            {
+                return bk.ToString(format);
+            }
+        }
+    }
+}
