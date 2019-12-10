0db70e8dc8f2ea24e9bd09a0b6366c7234cc6de5
= <anton.shheglov.1@gmail.com>
2019-12-10 12:30:09 +0300

    add: interface for loggers to use different frameworks, class NLogBookListLogger(use NLog framework).

---
+++ NET.W.2019.Shchahlou.12/BookServiceLogging/BookListLogger.cs
@@ -0,0 +1,60 @@
+﻿namespace NET.W._2019.Shchahlou._12
+{
+    using NLog;
+    using NLog.Fluent;
+
+    public interface IBookListLogger
+    {
+        public void Debug(string message);
+
+        public void Trace(string message);
+
+        public void Info(string message);
+
+        public void Warn(string message);
+
+        public void Error(string message);
+
+        public void Fatal(string message);
+    }
+
+    /// <summary>
+    /// Use NLog framework for logging
+    /// </summary>
+    public class NLogBookListLogger : IBookListLogger
+    {
+        private Logger logger = LogManager.GetLogger("Logger");
+
+        public void Debug(string message)
+        {
+            logger.Debug(message);
+        }
+
+        public void Error(string message)
+        {
+            logger.Error(message);
+        }
+
+        public void Fatal(string message)
+        {
+            logger.Fatal(message);
+        }
+
+        public void Info(string message)
+        {
+            logger.Info(message);
+        }
+
+        public void Trace(string message)
+        {
+            logger.Trace(message);
+        }
+
+        public void Warn(string message)
+        {
+            logger.Warn(message);
+        }
+    }
+}
+
+
