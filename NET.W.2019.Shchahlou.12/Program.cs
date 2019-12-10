cfa087c989b5542234d54cc6350676a794267972
= <anton.shheglov.1@gmail.com>
2019-12-10 06:15:41 +0300

    add: project. Initial commit for 12th day.

---
+++ NET.W.2019.Shchahlou.12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NET.W._2019.Shchahlou._12
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
