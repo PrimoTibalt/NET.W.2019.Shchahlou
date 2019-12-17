1ad45fce675b582d9694ca639751fc4c6d0802cf
= <anton.shheglov.1@gmail.com>
2019-12-17 14:39:40 +0300

    add: base skeleton of Queue.

--- NET.W.2019.Shchahlou.13/Queue/Queue.cs
+++ NET.W.2019.Shchahlou.13/Queue/Queue.cs
@@ -4,7 +4,44 @@ using System.Text;
 
 namespace NET.W._2019.Shchahlou._13
 {
-    public class Queue
+    public class Queue<T> : IEnumerable<T>, IReadOnlyCollection<T>, ICollection<T>
     {
+        /// <summary>
+        /// Initialize new instance of the class Queue<T>, which is empty
+        /// </summary>
+        public Queue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Queue(T parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Enqueue(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Contains(T value)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
