69993eb36561aacd7d03891b0d6b77d20e4bdf84
= <anton.shheglov.1@gmail.com>
2019-12-25 19:43:00 +0300

    add: node.

---
+++ NET.W.2019.Shchahlou.13/BinarySearchTree/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.W._2019.Shchahlou._13.BinarySearchTree
+{
+    public class Node<T>
+    {
+        public T value;
+        public Node<T> left;
+        public Node<T> right;
+    }
+}
