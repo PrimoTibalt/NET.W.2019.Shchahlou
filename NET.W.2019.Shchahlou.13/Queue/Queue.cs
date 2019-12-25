83f0388fafb3f3cb7f998c9d30479bb6d97a2551
= <anton.shheglov.1@gmail.com>
2019-12-25 11:50:52 +0300

    add: queue class.

--- NET.W.2019.Shchahlou.13/Queue/Queue.cs
+++ NET.W.2019.Shchahlou.13/Queue/Queue.cs
@@ -1,47 +1,92 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace NET.W._2019.Shchahlou._13
 {
-    public class Queue<T> : IEnumerable<T>, IReadOnlyCollection<T>, ICollection<T>
+    public class Queue<T> : IEnumerable<T>
     {
+        public int Count
+        {
+            get
+            {
+                return list.Count;
+            }
+        }
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        private List<T> list;
+
         /// <summary>
         /// Initialize new instance of the class Queue<T>, which is empty
         /// </summary>
         public Queue()
         {
-            throw new NotImplementedException();
+            list = new List<T>();
         }
 
-        public Queue(T parameters)
+        public Queue(IEnumerable<T> parameters) : base()
         {
-            throw new NotImplementedException();
+            if (parameters == null)
+            {
+                throw new ArgumentNullException("Value of parameter Queue cannot be null");
+            }
+            else
+            {
+                list = (List<T>)parameters; 
+            }
+            
         }
 
         public void Enqueue(T value)
         {
-            throw new NotImplementedException();
+            if (value == null)
+            {
+                throw new ArgumentNullException("Value of parameter for Queue cannot be null");
+            }
+            else
+            {
+                list.Add(value);
+            }
         }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            T value = list[0];
+            list.Remove(list[0]);
+            return value;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            return list[0];
         }
 
         public void Clear()
         {
+            list = new List<T>();
+        }
+
+        public bool Contains(T value)
+        {
+            return list.Contains(value);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
             throw new NotImplementedException();
         }
 
-        public void Contains(T value)
+        public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new Queue.QueueEnumerator<T>(list.ToArray());
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return list.GetEnumerator();
         }
     }
 }
