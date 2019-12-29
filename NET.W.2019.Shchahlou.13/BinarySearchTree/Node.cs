e164b4f9670e841a58fffb4fc15961d6f0e91b2c
= <anton.shheglov.1@gmail.com>
2019-12-29 19:25:45 +0300

    add: properties for fields and constructor to set value.

--- NET.W.2019.Shchahlou.13/BinarySearchTree/Node.cs
+++ NET.W.2019.Shchahlou.13/BinarySearchTree/Node.cs
@@ -13,16 +13,91 @@ namespace NET.W._2019.Shchahlou._13.BinarySearchTree
         /// <summary>
         /// The value of Node, to compare him with others.
         /// </summary>
-        public T value;
+        public T Value
+        {
+            get
+            {
+                return this.value;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Try to set null as value of the node.");
+                }
+                else
+                {
+                    this.value = value;
+                }
+            }
+        }
 
         /// <summary>
         /// Node with lower value
         /// </summary>
-        public Node<T> left;
+        public Node<T> Left
+        {
+            get
+            {
+                return this.left;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Try to set null as value of the left node.");
+                }
+                else
+                {
+                    this.left = value;
+                }
+            }
+        }
 
         /// <summary>
         /// Node with higer value
         /// </summary>
-        public Node<T> right;
+        public Node<T> Right
+        {
+            get
+            {
+                return this.right;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Try to set null as value of the right node.");
+                }
+                else
+                {
+                    this.right = value;
+                }
+            }
+        }
+
+        private T value;
+
+        private Node<T> left;
+
+        private Node<T> right;
+
+        /// <summary>
+        /// Sets the value for Node.
+        /// </summary>
+        /// <param name="val"></param>
+        public Node(T val)
+        {
+            if (val == null)
+            {
+                throw new ArgumentNullException("Throw null as value for node.");
+            }
+            else
+            {
+                this.Value = val;
+            }
+        }
     }
 }
