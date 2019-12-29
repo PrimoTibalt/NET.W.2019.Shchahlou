abc5ae2b4cbcb308d650ebd139607c5c3d8b99cf
= <anton.shheglov.1@gmail.com>
2019-12-29 12:43:06 +0300

    add: comments.

--- NET.W.2019.Shchahlou.13/BinarySearchTree/Node.cs
+++ NET.W.2019.Shchahlou.13/BinarySearchTree/Node.cs
@@ -4,10 +4,25 @@ using System.Text;
 
 namespace NET.W._2019.Shchahlou._13.BinarySearchTree
 {
+    /// <summary>
+    /// Type to represent node of the BinarySearchTree
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Node<T>
     {
+        /// <summary>
+        /// The value of Node, to compare him with others.
+        /// </summary>
         public T value;
+
+        /// <summary>
+        /// Node with lower value
+        /// </summary>
         public Node<T> left;
+
+        /// <summary>
+        /// Node with higer value
+        /// </summary>
         public Node<T> right;
     }
 }
