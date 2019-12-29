b762fefa06e4d1479096acb3e9ef85b6aefbf636
= <anton.shheglov.1@gmail.com>
2019-12-29 20:05:00 +0300

    add: comparers.

---
+++ NET.W.2019.Shchahlou.13/BinarySearchTree/Comparers/DefaultComparer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace NET.W._2019.Shchahlou._13.BinarySearchTree.Comparers
+{
+    public class DefaultComparer<T> : IComparer<T>
+    {
+        public int Compare([AllowNull] T x, [AllowNull] T y)
+        {
+            return x.GetHashCode().CompareTo(y.GetHashCode());
+        }
+    }
+}
---
+++ NET.W.2019.Shchahlou.13/BinarySearchTree/Comparers/StringComparer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace NET.W._2019.Shchahlou._13.BinarySearchTree.Comparers
+{
+    public class StringComparer<T> : IComparer<T>
+    {
+        public int Compare([AllowNull] T x, [AllowNull] T y)
+        {
+            return x.GetHashCode().ToString().CompareTo(y.GetHashCode().GetHashCode());
+        }
+    }
+}
