f968ddeed0a602e889d129c6ccdd5f1990a37204
= <anton.shheglov.1@gmail.com>
2019-12-29 19:55:37 +0300

    add: Insert method.

--- NET.W.2019.Shchahlou.13/BinarySearchTree/BinarySearchTree.cs
+++ NET.W.2019.Shchahlou.13/BinarySearchTree/BinarySearchTree.cs
@@ -12,62 +12,128 @@ namespace NET.W._2019.Shchahlou._13.BinarySearchTree
     public sealed class BinarySearchTree<T>
     {
         /// <summary>
+        /// To compare nodes.
+        /// </summary>
+        private IComparer<T> comparer;
+
+        /// <summary>
         /// Contains nodes of the Tree.
         /// </summary>
-        private List<Node<T>> elements;
+        private Node<T> root;
+
+        /// <summary>
+        /// Root of the tree.
+        /// </summary>
+        public Node<T> Root
+        {
+            get
+            {
+                return root;
+            }
+        }
 
         /// <summary>
         /// Initialize new Tree without nodes.
         /// </summary>
         public BinarySearchTree()
         {
-            elements = new List<Node<T>>();
+            this.root = null;
         }
 
         /// <summary>
         /// Initialize new Tree with one node.
         /// </summary>
         /// <param name="element"></param>
-        public BinarySearchTree(T element) : base()
+        public BinarySearchTree(T element) : this()
         {
-            this.Insert(element);
+            this.Insert(element, null);
         }
 
         /// <summary>
         /// Initialize new Tree with nodes from array.
         /// </summary>
         /// <param name="arrayOfElements"></param>
-        public BinarySearchTree(T[] arrayOfElements) : base()
+        public BinarySearchTree(T[] arrayOfElements) : this(arrayOfElements[0])
         {
             if (arrayOfElements == null)
             {
                 throw new ArgumentNullException("Passed null as argument(array) in constructor.");
             }
 
-            foreach (T element in arrayOfElements)
+            for (int i = 1; i < arrayOfElements.Length; i++)
             {
-                this.Insert(element);
+                // i = 1 because we already have arrayOfElements[0] in List.
+                this.Insert(arrayOfElements[i], this.Root);
             }
         }
+
+        /// <summary>
+        /// Sets comparer to insert element in the tree.
+        /// </summary>
+        /// <param name="newComparer"></param>
+        public void SetComparer(IComparer<T> newComparer)
+        {
+            if (newComparer == null)
+            {
+                throw new ArgumentNullException("Throw null as comparer.");
+            }
+
+            this.comparer = newComparer;
+        }
+
+        /// <summary>
+        /// It was easyiest way to don't give chance to user change root.
+        /// </summary>
+        /// <param name="element"></param>
+        public void Insert(T element) => this.Insert(element, this.root);
 
         /// <summary>
         /// Inserts one element(node) in Tree.
         /// </summary>
         /// <param name="element"></param>
-        public void Insert(T element)
+        /// <param name="node"></param>
+        private void Insert(T element, Node<T> node)
         {
             if (element == null)
             {
                 throw new ArgumentNullException("Passed null as argument in constructor.");
             }
 
-            if (elements.Count > 0)
+            if (node == null)
             {
-                // TO DO : call of function to Tree traversal.
+                this.root = new Node<T>(element);
             }
             else
             {
-                elements.Add(new Node<T> { value = element });
+                switch (comparer.Compare(element, node.Value))
+                {
+                    case 1:
+                        if (node.Right == null)
+                        {
+                            node.Right = new Node<T>(element);
+                        }
+                        else
+                        {
+                            this.Insert(element, node.Right);
+                        }
+
+                        break;
+                    case 0:
+                        throw new ArgumentException("Already have this element in the Tree.");
+                    case -1:
+                        if (node.Left == null)
+                        {
+                            node.Left = new Node<T>(element);
+                        }
+                        else
+                        {
+                            this.Insert(element, node.Left);
+                        }
+
+                        break;
+                    default:
+                        throw new SystemException("Comparer throw strange result.(not 1 or 0 or -1.");
+                }
             }
         }
 
