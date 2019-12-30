21d6aed3659e12fad2ddc3fdeaab06d9e3a7615f
= <anton.shheglov.1@gmail.com>
2019-12-30 18:25:05 +0300

    fix: before it compares hashcode of string to hashcode of hashcode, now it compares hashcodes of 2 strings.

--- NET.W.2019.Shchahlou.13/BinarySearchTree/Comparers/StringComparer.cs
+++ NET.W.2019.Shchahlou.13/BinarySearchTree/Comparers/StringComparer.cs
@@ -9,7 +9,7 @@ namespace NET.W._2019.Shchahlou._13.BinarySearchTree.Comparers
     {
         public int Compare([AllowNull] T x, [AllowNull] T y)
         {
-            return x.GetHashCode().ToString().CompareTo(y.GetHashCode().GetHashCode());
+            return x.GetHashCode().ToString().CompareTo(y.ToString().GetHashCode());
         }
     }
 }
