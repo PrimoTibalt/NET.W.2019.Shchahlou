e410ae3c7580a113f5c3f9b3cd404c583ea1feae
= <anton.shheglov.1@gmail.com>
2019-12-30 13:41:38 +0300

    add: Book and Pointer to test BinarySearchTree.

---
+++ NET.W.2019.Shchahlou.13/BinarySearchTree/Comparers/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.W._2019.Shchahlou._13.BinarySearchTree.Comparers
+{
+    public class Book : IComparable
+    {
+        public int Year { get; set; }
+
+        public Book(int year)
+        {
+            this.Year = year;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Book))
+            {
+                throw new ArgumentException("Try to compare Pointer with non Book.");
+            }
+
+            Book second = obj as Book;
+            return this.Year.CompareTo(second.Year);
+        }
+    }
+}
---
+++ NET.W.2019.Shchahlou.13/BinarySearchTree/Comparers/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.W._2019.Shchahlou._13.BinarySearchTree.Comparers
+{
+    public struct Point : IComparable
+    {
+        public int Number { get; set; }
+
+        public Point(int num)
+        {
+            this.Number = num;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Point))
+            {
+                throw new ArgumentException("Try to compare Pointer with non pointer.");
+            }
+
+            Point second = (Point) obj;
+            return this.Number.CompareTo(second.Number);
+        }
+    }
+}
