ec5a42e468a4ed8372ace5fedec1115f726db9a7
= <anton.shheglov.1@gmail.com>
2019-12-30 13:32:44 +0300

    add: tests for Queue class.

--- NET.W.2019.Shchahlou.13/Tests/TestDay13/TestQueue.cs
+++ NET.W.2019.Shchahlou.13/Tests/TestDay13/TestQueue.cs
@@ -1,13 +1,30 @@
 using NUnit.Framework;
+using System;
+using NET.W._2019.Shchahlou._13;
 
 namespace TestDay13
 {
-    public class Tests
+    [TestFixture]
+    public class TestsQueue
     {
-        [Test]
-        public void Test1()
+        [TestCase(new object[] { "Sivir", "Ahri", "Malzahar", "Kindred", "al;sh830", "Clown" }, ExpectedResult = new object[] { "Sivir", "Ahri", "Malzahar", "Kindred", "al;sh830", "Clown" })]
+        [TestCase(new object[] { 0, 1, 2, 3, -1, -2, -3, int.MaxValue, int.MinValue }, ExpectedResult = new object[] { 0, 1, 2, 3, -1, -2, -3, int.MaxValue, int.MinValue })]
+        public object[] Test_Queue(object[] arr)
         {
-            Assert.Pass();
+            Queue<object> queue = new Queue<object>();
+            for (int i = 0; i < arr.Length; i++) 
+            {
+                queue.Enqueue(arr[i]);
+            }
+
+            object[] arrNew = new object[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arrNew[i] = queue.Dequeue();
+            }
+
+            return arrNew;
         }
     }
 }
