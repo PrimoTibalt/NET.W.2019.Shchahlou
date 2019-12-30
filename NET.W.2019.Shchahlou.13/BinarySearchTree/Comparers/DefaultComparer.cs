f77112c3ed58a35ac6324875914e4129f76c743a
= <anton.shheglov.1@gmail.com>
2019-12-30 14:33:10 +0300

    fix: deffault comparer now can use Compare of IComparable classes.

--- NET.W.2019.Shchahlou.13/BinarySearchTree/Comparers/DefaultComparer.cs
+++ NET.W.2019.Shchahlou.13/BinarySearchTree/Comparers/DefaultComparer.cs
@@ -5,10 +5,25 @@ using System.Text;
 
 namespace NET.W._2019.Shchahlou._13.BinarySearchTree.Comparers
 {
+    /// <summary>
+    /// Have only compare method, that compares by standart comparer of the type(if exists) if comparer doesnt exist, than compares HashCode(like int).
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class DefaultComparer<T> : IComparer<T>
     {
+        /// <summary>
+        /// Compares by standart comparer of the type(if exists) if comparer doesnt exist, than compares HashCode(like int).
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public int Compare([AllowNull] T x, [AllowNull] T y)
         {
+            if (x is IComparable first && y is IComparable second)
+            {
+                return first.CompareTo(second);
+            }
+
             return x.GetHashCode().CompareTo(y.GetHashCode());
         }
     }
