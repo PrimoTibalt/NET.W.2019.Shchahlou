e0e69d3e48c3b22d015c0a6de176891b226c4363
= <anton.shheglov.1@gmail.com>
2020-01-07 15:12:44 +0300

    add: test in console.

--- NET.W.2019.Shchahlou.17/Program.cs
+++ NET.W.2019.Shchahlou.17/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace NET.W._2019.Shchahlou._17
 {
     using System;
+    using System.IO;
     using System.Text;
     using NET.W._2019.Shchahlou._17.XML;
     using NET.W._2019.Shchahlou._17.XML.Interfaces;
@@ -11,9 +12,11 @@
         {
             FileReader re = new FileReader(@"C:\Users\PrimoTibalt\Desktop\Studying\NET.W.2019.Shchahlou\NET.W.2019.Shchahlou.17\text.txt");
             string[] text = re.ReadAll();
+            Console.WriteLine("URLs for test: ");
             StringBuilder bldr = new StringBuilder();
             foreach(string str in text)
             {
+                Console.WriteLine(str);
                 bldr.AppendLine(str);
             }
 
@@ -21,7 +24,13 @@
             StringToXml converter = new StringToXml();
             XMLWriter writer = new XMLWriter(val, converter, "default.xml");
             writer.Write(bldr.ToString());
-
+            Console.WriteLine("The final XML File : ");
+            string xml;
+            using (StreamReader reader = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "default.xml"))
+            {
+                xml = reader.ReadToEnd();
+            }
+            Console.WriteLine(xml);
         }
     }
 }
